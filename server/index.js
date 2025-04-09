b7cf63054cfa37e4b89052172addb4b669ba656b
Ashish Mittal <aashishrbmittal@gmail.com>
2025-04-10 00:02:54 +0530

    final

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: ['http://localhost:5173','http'], // Allow these origins to make requests
+    origin: ['http://localhost:5173','https://nex-times-beyond-headlines-tp8t.vercel.app/','http'], // Allow these origins to make requests
     methods: ["POST", "GET", "OPTIONS"], // Allow these HTTP methods
     credentials: true, // Allow cookies and other credentials to be sent
     allowedHeaders: ["Content-Type", "Authorization"], // Allow the token header
