5b5f37334f7ac18b49405de4270603b64758d626
Ashish Mittal <aashishrbmittal@gmail.com>
2025-04-09 23:53:14 +0530

    initial

--- server/index.js
+++ server/index.js
@@ -5,8 +5,12 @@ import newsRoutes from './routes/newsRoutes.js';
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+    origin: ['http://localhost:5173','http'], // Allow these origins to make requests
+    methods: ["POST", "GET", "OPTIONS"], // Allow these HTTP methods
+    credentials: true, // Allow cookies and other credentials to be sent
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow the token header
+}));app.use(express.json());
 
 app.use('/api/news', newsRoutes);
 
